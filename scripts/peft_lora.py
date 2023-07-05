9d895cc2ccc4d91c48039735cfae351edddcbb2c
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-07-05 19:43:44 +0530

    Update peft_lora.py

--- scripts/peft_lora.py
+++ scripts/peft_lora.py
@@ -105,7 +105,7 @@ def convert_unet_state_dict(unet_state_dict):
         for sd_part, hf_part in unet_conversion_map_layer:
             v = v.replace(hf_part, sd_part)
         mapping[k] = v
-    new_state_dict = {v: unet_state_dict[k] for k, v in mapping.items()}
+    new_state_dict = {v: unet_state_dict[k] for k, v in mapping.items() if k in unet_state_dict}
     return new_state_dict
 
 
